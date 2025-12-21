c1c32b56ffea13e02d5d28422204606bc408b6bc
hasan00232 <[mohn4897@gmail.com]>
2025-12-21 18:51:16 +0300

    .

--- Domain/Entites/Models/User.cs
+++ Domain/Entites/Models/User.cs
@@ -13,6 +13,7 @@ namespace Domain.Entites.Models
         public string Email { get; set; }
         [Required]
         [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,24}$",ErrorMessage = "Invalid Password.")]
         public string Password{ get; set; }
         [Required]
         [RegularExpression(@"^(?:\+962|00962|0)7[789]\d{7}$", ErrorMessage = "Invalid Phone Number.")]
